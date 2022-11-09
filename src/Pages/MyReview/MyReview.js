5101e65e336e0835804b22815a1572bdc9fdac19
sagorsikder <mdsagorsikder97@gmail.com>
2022-11-10 02:17:37 +0600

    yreview almost complete

--- src/Pages/MyReview/MyReview.js
+++ src/Pages/MyReview/MyReview.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import {React,useContext,useEffect,useState} from 'react';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 
 const MyReview = () => {
+    const {user,logOut} = useContext(AuthContext);
+    const [review,setReview]=useState({})
+
+    console.log(review)
+    useEffect(()=>{
+     
+        fetch(`http://localhost:5000/myreview?email=${user.email}`)
+        .then(data => setReview(data))
+        .catch(err=>console.log(err))
+    },[user.email,logOut])
     return (
-        <div>
-            <h1>this is my reviews page.</h1>
+        <div className='w-3/4 mx-auto'>
+            <h1 className='text-center text-5xl font-semibold bg-primary p-3 text-warning'> My reviews</h1>
+            {
+                user?.email
+            }
         </div>
     );
 };
